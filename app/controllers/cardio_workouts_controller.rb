97f507ccf541427c9fe9ac76c3844253d14e784c
Karl Loomes <kloomes@hotmail.com>
2020-04-15 16:25:56 +0100

    Cardio show page added

--- app/controllers/cardio_workouts_controller.rb
+++ app/controllers/cardio_workouts_controller.rb
@@ -2,4 +2,8 @@ class CardioWorkoutsController < ApplicationController
   def index
     @cardio_workouts = CardioWorkout.where(user_id: current_user.id)
   end
+
+  def show
+    @cardio_workout = CardioWorkout.find(params[:id])
+  end
 end
