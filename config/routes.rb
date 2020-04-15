e8ab6a046e0285a62b10e55a6e41d6ed4dbb6199
Karl Loomes <kloomes@hotmail.com>
2020-04-15 12:28:07 +0100

    User info added to show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :update] do
+  resources :users, only: [:show] do
     resources :stats
     resources :cardio_workouts
     resources :weights_workouts do
