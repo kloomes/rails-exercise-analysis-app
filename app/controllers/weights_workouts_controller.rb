3b897a45af76f89345c172cf274e5a09d1a7c87b
Karl Loomes <kloomes@hotmail.com>
2020-04-16 14:45:11 +0100

    Weights new and create added

--- app/controllers/weights_workouts_controller.rb
+++ app/controllers/weights_workouts_controller.rb
@@ -2,4 +2,36 @@ class WeightsWorkoutsController < ApplicationController
   def index
     @weights_workouts = WeightsWorkout.where(user_id: current_user.id)
   end
+
+  def new
+    @weights_workout = WeightsWorkout.new
+  end
+
+  def create
+    @weights_workout = WeightsWorkout.new(weights_workout_params)
+    @user = User.find(params[:user_id])
+    @weights_workout[:user_id] = @user.id
+    if @weights_workout.save
+      redirect_to user_weights_workouts_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def weights_workout_params
+    params.require(:weights_workout).permit(
+      :workout_type,
+      :date,
+      :time,
+      :duration_hours,
+      :duration_minutes,
+      :duration_seconds,
+      :avg_hr,
+      :max_hr,
+      :kcal,
+      :notes
+      )
+  end
 end
