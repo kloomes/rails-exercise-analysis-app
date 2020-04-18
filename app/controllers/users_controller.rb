6a970cedd39793cd36bb5bd492c8be02cde59fb3
Karl Loomes <kloomes@hotmail.com>
2020-04-18 18:04:21 +0100

    Last 10 workouts added to profile page, container in application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @workouts = CardioWorkout.order(:date).last(5) + WeightsWorkout.order(:date).last(5)
+    @results = @workouts.sort_by { |obj| obj.date }
+    @results.sort! { |x, y| y.date <=> x.date }
   end
-
 end
